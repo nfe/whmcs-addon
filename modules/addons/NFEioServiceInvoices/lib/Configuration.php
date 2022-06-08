7b63be1b79fc28ebca18aa68f3a388141cb632b6
Andre Belafronte <andre@eunarede.com>
2022-06-07 21:21:54 -0300

    bump: v2.1.3

--- modules/addons/NFEioServiceInvoices/lib/Configuration.php
+++ modules/addons/NFEioServiceInvoices/lib/Configuration.php
@@ -23,7 +23,7 @@ final class Configuration extends \WHMCSExpert\mtLibs\process\AbstractConfigurat
 
     private $encryptHash = '';
 
-    public $version = '2.1.2';
+    public $version = '2.1.3';
 
     public $tablePrefix = 'mod_nfeio_si_';
 
